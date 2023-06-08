afd271f429366ba892943472b0752e0ebfd23f67
vPere <pvidiella@edu.tecnocampus.cat>
2023-06-09 01:03:50 +0200

    Adds i18n auto translations. --> TODO: Make the navbar useful

---
+++ i18n-loader.js
@@ -0,0 +1,32 @@
+i18next.use(i18nextXHRBackend).init({
+    lng: getLang(),
+    fallbackLng: 'ca',
+    backend: {
+        loadPath: '/i18n/{{lng}}.json'
+    }
+}, function (err, t) {
+    if (err) {
+        console.error('Failed to load translations:', err);
+        return;
+    }
+
+    const translateElements = document.querySelectorAll('[data-i18n]');
+    translateElements.forEach(element => {
+        const translationKey = element.getAttribute('data-i18n');
+        element.textContent = t(translationKey);
+    });
+});
+
+function getLang() {
+    if (navigator.languages !== undefined)
+        return checkLangAvailable(navigator.languages[0].toLocaleLowerCase().substring(0, 2));
+    return checkLangAvailable(navigator.language.toLocaleLowerCase().substring(0, 2));
+}
+
+function checkLangAvailable(lang) {
+    const availableLocales = ['ca', 'es', 'en'];
+    if (!availableLocales.includes(lang)) {
+        return 'en';
+    }
+    return lang;
+}
